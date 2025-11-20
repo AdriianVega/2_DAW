c9c5ef6a8aec67ef0124600a965cbf84a5aaaa1e
Adrian Vega <aadriianvega@gmail.com>
2025-11-20 13:41:39 +0100

    Cliente BOM

---
+++ DWC/BOM/Ejercicio1/script.js
@@ -0,0 +1,5 @@
+location.href = "www.google.com";
+
+var ventana = window.open(location.href, "_blank", "resizable=yes, width=500,height=100, left=100, top=200");
+
+console.log(window.location.href);
