ad844dd899e0ea4f848b563bbd94081b88407fca
Adrián Nataniel Vega Pérez <aadriianvega@gmail.com>
2025-12-17 12:05:34 +0100

    Comienzo de ejercicios DIW y DWC

---
+++ DWC/2º Trimestre/Eventos/burbustop.js
@@ -0,0 +1,6 @@
+// Ejercicio 1
+
+const nuevoDiv = document.createElement("div")
+
+nuevoDiv.setAttribute("onclick", "alert('Pulsaste sobre el botón')")
+nuevoDiv.innerHTML = "<button> Pulsa aquí </button>";
