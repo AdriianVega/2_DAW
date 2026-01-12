9dd6788d2b33e495c275d464af3756ac2acc3e47
Adrián Nataniel Vega Pérez <aadriianvega@gmail.com>
2026-01-12 20:21:17 +0100

    Animacion en el menu de DWS Aplicada

--- DWS/UD4/Proyecto/Admin/js/secciones_menu.js
+++ DWS/UD4/Proyecto/Admin/js/secciones_menu.js
@@ -1,12 +1,11 @@
 const main = document.getElementsByTagName("main")[0];
 
 const secciones = [
-    { titulo: "Productos", imagen: "imagen.svg", ruta: "inicio.php"},
-    { titulo: "Clientes", imagen: "imagen.svg", ruta: "inicio.php"},
-    { titulo: "Productos", imagen: "imagen.svg", ruta: "inicio.php"},
-    { titulo: "Productos", imagen: "imagen.svg", ruta: "inicio.php"},
-    { titulo: "Productos", imagen: "imagen.svg", ruta: "inicio.php"},
-    { titulo: "Configuración", imagen: "imagen.svg", ruta: "inicio.php"},
+    { titulo: "Productos", imagen: "../img/menu/caja.svg", ruta: "inicio.php"},
+    { titulo: "Clientes", imagen: "../img/menu/personas.svg", ruta: "inicio.php"},
+    { titulo: "Pedidos", imagen: "../img/menu/entrega-de-pedidos.svg", ruta: "inicio.php"},
+    { titulo: "Categorías", imagen: "../img/menu/lista.svg", ruta: "inicio.php"},
+    { titulo: "Configuración", imagen: "../img/menu/ajuste.svg", ruta: "inicio.php"},
 ]
 
 for (let seccion of secciones) {
@@ -15,7 +14,7 @@ for (let seccion of secciones) {
     enlace.href = seccion.ruta;
 
     const nuevaSeccion = document.createElement("article");
-    nuevaSeccion.style.border = "1px solid black";
+    nuevaSeccion.style.border = "3px solid blue";
 
     const imagen = document.createElement("img");
     imagen.src = seccion.imagen
