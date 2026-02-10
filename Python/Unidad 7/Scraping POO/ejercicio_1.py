c8ebf2cc1b4960b47d777ff21006aafead0900fc
Adrián Nataniel Vega Pérez <aadriianvega@gmail.com>
2026-02-10 12:16:47 +0100

    Mini Fix DIW y reestructuración carpetas Python

---
+++ Python/Unidad 7/Scraping POO/ejercicio_1.py
@@ -0,0 +1,12 @@
+"""
+    Ejercicio 1
+"""
+
+
+class Book:
+    # D
+    def __init__(self, titulo, precio, disponible, url):
+        self.titulo = titulo
+        self.precio = precio
+        self.disponible = disponible
+        self.url = url
